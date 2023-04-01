44025777079a1d9a0b270f661b23ad20745440bd
Anthony Shew <anthony.shew@vercel.com>
2023-04-01 01:04:16 -0700

    Build canary versions with code.

---
+++ scripts/incrementCanary.js
@@ -0,0 +1,36 @@
+const execSync = require('child_process').execSync
+
+const getNextVersion = () => {
+  const buffer = execSync("pnpm view @ashew/turboviz dist-tags")
+
+  const toString = buffer.toString().replace(
+    "\n", ""
+  )
+    .replaceAll("'", "\"")
+    .replace("next", '"next"')
+    .replace("latest", '"latest"')
+    .replace(" ", "")
+
+  const result = JSON.parse(toString)
+  return result.next
+}
+
+const incrementCanary = (versionString) => {
+  const splitted = versionString.split(".")
+  const lastIndex = splitted.length - 1
+  const currentVersion = splitted[lastIndex]
+  const nextTag = Number(currentVersion) + 1
+  const nextCanary = splitted.slice(0, -1).concat(nextTag).join(".")
+
+  return nextCanary
+
+}
+
+const main = () => {
+  const npmNextVersion = getNextVersion()
+  console.log({ npmNextVersion })
+  const nextCanary = incrementCanary(npmNextVersion)
+  console.log({ nextCanary })
+}
+
+main()
