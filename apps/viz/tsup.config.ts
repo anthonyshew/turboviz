0a0018fc85b1d9364ab03333345ead2ede974fd4
Anthony Shew <anthony.shew@vercel.com>
2023-04-11 22:50:40 -0700

    Fix outDir.

--- apps/viz/tsup.config.ts
+++ apps/viz/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entry: ["bin.ts"],
   format: ["cjs"],
-  "outDir": "disty",
   minify: true,
   ...options,
 }));
